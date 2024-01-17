b2dc9d2681a01dec6737b9116ba9349538ebae41
Maer Melo <salalbr@gmail.com>
2024-01-17 00:26:14 -0800

    Initial commit - creating skeleton

---
+++ resource-mgmt/outputs.tf
@@ -0,0 +1,7 @@
+output "compartment_id" {
+  value = oci_identity_compartment.compartment.id
+}
+
+output "compartment_name" {
+  value = oci_identity_compartment.compartment.name
+}
---
+++ resource-mgmt/variables.tf
@@ -0,0 +1,14 @@
+variable "parent_compartment_id" {
+  type        = string
+  description = "The OCID of the parent compartment containing the compartment"
+}
+
+variable "description" {
+  type        = string
+  description = "The description you assign to the compartment"
+}
+
+variable "name" {
+  type        = string
+  description = "The name you assign to the compartment"
+}
